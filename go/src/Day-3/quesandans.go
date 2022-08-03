31123b3369b02bb113cb3078260cbc2b1c9104e5
Pranav Konde <pranavkonde2020@gmail.com>
2022-08-03 12:35:48 +0530

    Day 3  Code Example

---
+++ go/src/Day-3/quesandans.go
@@ -0,0 +1,19 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	// a := make([]int, 0, 0)
+	// fmt.Println(len(a), cap(a))
+	// fmt.Println(a)
+	// a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	// b := a[:2]
+	// fmt.Println(len(b), cap(b))
+	// d := a[2:5]
+	// fmt.Println(len(d), cap(d))
+	a := make([]int, 5)
+	fmt.Println(len(a), cap(a))
+
+}
---
+++ go/src/Day-3/sliceusingmain.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice := make([]int, 2, 5)
+	fmt.Println(slice)
+	var slice1 []int
+	fmt.Println(slice1)
+}
